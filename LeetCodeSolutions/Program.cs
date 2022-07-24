b9a2f0ae9934b38c4d626d4e43f2fe176b3a95ba
Ali İhsan YILDIZ <46348313+FeelGD@users.noreply.github.com>
2022-07-24 15:18:16 -0400

    Oklid

--- LeetCodeSolutions/Program.cs
+++ LeetCodeSolutions/Program.cs
@@ -221,7 +221,14 @@ namespace LeetCodeSolutions
 
             }
 
-
+            int Oklid(int num1, int num2)
+            {
+                if (num2==0)
+                {
+                    return num1;
+                }
+                return Oklid(num2, num1 % num2);
+            }
 
 
 
