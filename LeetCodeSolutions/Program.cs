b46c09a001618b55e1eeb3e68b82f2c44cffe9da
Ali İhsan YILDIZ <46348313+FeelGD@users.noreply.github.com>
2022-07-24 16:10:20 -0400

    IsSameTree

--- LeetCodeSolutions/Program.cs
+++ LeetCodeSolutions/Program.cs
@@ -17,6 +17,18 @@ namespace LeetCodeSolutions
             this.next = next;
         }
     }
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
     public class Solutions
     {
         static void Main(string[] args)
@@ -294,6 +306,27 @@ namespace LeetCodeSolutions
 
 
             }
+
+            bool IsSameTree(TreeNode p, TreeNode q)
+            {
+
+                if (p == null && q == null) return true;
+                if (p == null || q == null) return false;
+                if (p.val != q.val) return false;
+                return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
 
             #region functionCalls   
             #region TwoSum
